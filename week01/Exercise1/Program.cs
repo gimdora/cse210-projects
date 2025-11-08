ee2f381494aae3b42de597a7a43c0462b512ae90
gimdora <hkim2@byupathway.edu>
2025-11-08 23:30:33 +0900

    Finished prep 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        first_name = textInfo.ToTitleCase(first_name.ToLower());
+        last_name = textInfo.ToTitleCase(last_name.ToLower());
+
+        Console.WriteLine();
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        float grade_flt = float.Parse(grade);
+
+        if (grade_flt >= 90)
+            Console.WriteLine("A");
+        else if (grade_flt >= 80)
+            Console.WriteLine("B");
+        else if (grade_flt >= 70)
+            Console.WriteLine("C");
+        else if (grade_flt >= 60)
+            Console.WriteLine("D");
+        else if (grade_flt < 60)
+            Console.WriteLine("F");
+
+        if (grade_flt >= 70)
+            Console.WriteLine("Congratulations! You have passed!");
+        if (grade_flt < 70)
+            Console.WriteLine("You have not passed. Don't worry, you can do better next time!");
+
+        Console.Write($"{grade_flt}");
     }
 }
