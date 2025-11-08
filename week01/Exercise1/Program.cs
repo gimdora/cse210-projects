9194f8c82e5c845c8b70ce70b5a078e17f0c5f2b
gimdora <hkim2@byupathway.edu>
2025-11-08 23:32:18 +0900

    description added

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,6 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user for their name.
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
         
