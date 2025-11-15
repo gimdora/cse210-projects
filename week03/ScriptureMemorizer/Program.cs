4b15594796cf81bba6c1d770193f9b93b46f5bd4
gimdora <hkim2@byupathway.edu>
2025-11-15 15:46:09 +0900

    vscode preferation

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -6,12 +6,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Reference reference = new("Proverbs", 3, 5, 6);
         string text =
             "Trust in the Lord with all thine heart; and lean not unto thine own understanding. " +
             "In all thy ways acknowledge him, and he shall direct thy paths.";
 
-        Scripture scripture = new Scripture(reference, text);
+        Scripture scripture = new(reference, text);
 
         while (true)
         {
@@ -22,7 +22,7 @@ class Program
             string input = Console.ReadLine();
 
             if (!string.IsNullOrWhiteSpace(input) &&
-                input.Trim().ToLower() == "quit")
+                input.Trim().Equals("quit", StringComparison.CurrentCultureIgnoreCase))
             {
                 break;
             }
